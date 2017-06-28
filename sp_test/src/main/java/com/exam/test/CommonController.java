c422cf46aa6c0212b2e9c21b97c0e28eb999ce10
402 <402@192.168.101.78>
2017-06-28 21:19:50 +0900

    commit <2017-06-28>

--- sp_test/src/main/java/com/exam/test/CommonController.java
+++ sp_test/src/main/java/com/exam/test/CommonController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-@RequestMapping("/")
 public class CommonController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(CommonController.class);
