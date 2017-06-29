bb1fb0ed4fb769bd62fed7de2e92ec175ef14295
402 <402@192.168.101.78>
2017-06-29 22:19:18 +0900

    commit <2017-06-29>

---
+++ sp_test/src/main/java/com/exam/test/HomeController.java
@@ -0,0 +1,26 @@
+package com.exam.test;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/board")
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		model.addAttribute("test", "모델에 테스트라고 넣으셨군요" );
+		return "main";
+	}
+	
+	
+}
---
+++ sp_test/src/main/webapp/WEB-INF/views/user/login.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<script>
+function movePage(){
+	location.href='http://localhost:8080/test/main';
+}
+</script>
+<body>
+<p>로그인 화면입니다.</p>
+<input type="button" value="홈으로" onclick="movePage()"/>
+</body>
+</html>
