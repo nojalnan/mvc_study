6c51a01c7de27cca86a7329363c04aca497e4c73
nojalnan <nojalnan@nate.com>
2017-07-02 20:55:15 +0900

    commit <2017-07-02>

--- sp_m/src/main/java/com/test/ioc/exam/Work.java
+++ sp_m/src/main/java/com/test/ioc/exam/Work.java
@@ -13,16 +13,12 @@ public class Work {
 	@Autowired
 	List<Emp> empList;
 	public Work(){
-//		empList = new ArrayList<Emp>();
-//		empList.add(new Programmer());
-//		empList.add(new Designer());
+
 	}
 
 	public static void main(String[] args) {
 		ApplicationContext factory = new ClassPathXmlApplicationContext("classpath:ioc3.xml");
 		Work w = (Work) factory.getBean("work");
-		
-//		Work w = new Work();
 		
 		for (Emp e : w.empList) {
 			e.work();
