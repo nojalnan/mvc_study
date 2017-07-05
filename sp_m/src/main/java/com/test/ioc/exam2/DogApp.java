d2e5b889775bb9169c60a432075c1ecf099757eb
Nows <Nows@Nows-PC>
2017-07-05 17:33:21 +0900

    commit <2017-07-05>

--- sp_m/src/main/java/com/test/ioc/exam2/DogApp.java
+++ sp_m/src/main/java/com/test/ioc/exam2/DogApp.java
@@ -5,8 +5,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DogApp {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"dog/dog.xml", "dog/dog_bean.xml"});
-		DogManager dogManager = (DogManager) context.getBean("dogManager");
+//		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"dog/dog.xml", "dog/dog_bean.xml"});
+//		DogManager dogManager = (DogManager) context.getBean("dogManager");
+//		dogManager.bark();
+		
+		DogManager dogManager = new DogManager();
+		Jindo jindo = new Jindo();
+		Pudle pudle = new Pudle();
+		dogManager.setJindo(jindo);
+		dogManager.setPudle(pudle);
 		dogManager.bark();
 	}
 
