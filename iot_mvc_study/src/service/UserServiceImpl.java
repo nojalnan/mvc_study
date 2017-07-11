a976e20cc6c626f9767184d219f7f4dde0222368
402 <402@DESKTOP-B3HQL0V>
2017-07-11 20:47:02 +0900

    commit

--- iot_mvc_study/src/service/UserServiceImpl.java
+++ iot_mvc_study/src/service/UserServiceImpl.java
@@ -4,24 +4,23 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
+import bean.User;
 import common.DBConn;
 
 public class UserServiceImpl implements UserServiceIn {
-	
+	Scanner scan = new Scanner(System.in);
 	Connection con;
 	PreparedStatement ps;
 	
 	UserServiceImpl(){
 		try {
 			con = DBConn.getCon();
-		} catch (ClassNotFoundException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} 
 	}
 
 	@Override
@@ -33,11 +32,15 @@ public class UserServiceImpl implements UserServiceIn {
 //		getMetaData
 //		getColumnCount
 	}
+	
+	public void insertUserInfo(User user){
+		
+	}
 
 	@Override
 	public void insertUser() {
-		// TODO Auto-generated method stub
-		
+		String sql = "insert into user_info(user_id, user_name, user_pwd, class_num, age)";
+		sql += "values(?,?,?,?,?)";
 	}
 
 	@Override
